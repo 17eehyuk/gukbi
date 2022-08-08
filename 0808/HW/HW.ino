90ca321dcb9f53548642c1d67470d17096850a7c
17eehyuk <17eehyuk@gmail.com>
2022-08-08 17:30:25 +0900

    2022-08-08_17:30:25.11

--- 0808/HW/HW.ino
+++ 0808/HW/HW.ino
@@ -1,7 +1,6 @@
 #include <MultiFuncShield.h>
 
 int count = 0;
-int btn_flag = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -16,37 +15,12 @@ void loop() {
   byte btn_num = btn & B00111111;
   byte btn_action = btn & B11000000;
 
-  if(btn_num == 1 && btn_flag == 0)
+
+  if(btn_action == BUTTON_PRESSED_IND || btn_action == BUTTON_LONG_PRESSED_IND)
   {
-    btn_flag = 1;
-    if(count<9999){count++;}     
-  }
-  else if(btn_num == 2 && btn_flag == 0)
-  {
-    btn_flag = 1;
-    if(count>0){count--;}
-  }
-  else if(btn_num == 3 && btn_flag == 0)
-  {
-    btn_flag = 1;
-    count = 0; 
+    if(btn_num == 1){if(count<9999){count++;}}
+    else if(btn_num == 2){if(count>0){count--;}}
+    else if(btn_num == 3){count = 0;}
   }
 
-  if(btn_action == BUTTON_SHORT_RELEASE_IND || btn_action == BUTTON_LONG_RELEASE_IND)
-  {
-    btn_flag = 0;   // 때는 순간 flag 초기화
-  }
-
-  if (btn_action == BUTTON_LONG_PRESSED_IND)      // 꾹 눌렀을 때
-  {
-    if(btn_num ==1)
-    {
-      count++;
-    }
-    else if(btn_num ==2)
-    {
-      count--;
-    }
-  }
-  
 }
